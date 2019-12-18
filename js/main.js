3dabab22955ecc6041e6a6021efebf4ad0776183
= <=>
2019-12-18 14:24:55 +0300

    minimise js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,1 @@
-$(function(){
-
-  $('.slider__inner, .news__slider-inner').slick({
-    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-    infinite: false
-  });
-
-  $('select').styler();
-
-  $('.header__btn-menu').on('click', function(){
-    $('.menu ul').slideToggle();
-  });
-
-});
+$(function(){$(".slider__inner, .news__slider-inner").slick({nextArrow:'<button type="button" class="slick-btn slick-next"></button>',prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',infinite:!1}),$("select").styler(),$(".header__btn-menu").on("click",function(){$(".menu ul").slideToggle()})});
